67d18a9524058e1412dc2eb7f816aac28abac1ab
recuerdo <84432844+mirecuerdo@users.noreply.github.com>
2021-08-15 09:34:06 +0530

    Add files via upload

---
+++ allpassword.js
@@ -0,0 +1,6 @@
+a=prompt("Enter Password");
+if(a==7662)
+ alert("Go Ahead....");
+ 
+else if(a!=7662)
+ window.location="error.html";
---
+++ mainpassword.js
@@ -0,0 +1,7 @@
+a=prompt("Enter Password");
+   var b=prompt("Enter Your Name");
+        if(a==566676)
+            alert("Welcome "+ b );
+            
+        else if(a!=566676)
+            window.location="error.html";
